2d469e0d9b22e9c402a781806b792ebc4dafed47
AksPatil <patilakshaya86@gmail.com>
2022-06-08 16:22:06 +0530

    HRM04: Resolved Review comments

--- app/src/main/java/com/example/wearableapp/presentation/adapter/MainMenuAdapter.kt
+++ app/src/main/java/com/example/wearableapp/presentation/adapter/MainMenuAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wearableapp.R
 import com.example.wearableapp.presentation.model.MenuItem
@@ -78,7 +79,7 @@ class MainMenuAdapter() : RecyclerView.Adapter<MainMenuAdapter.RecyclerViewHolde
     override fun getItemCount() = dataSource.size
 
     class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var menuContainer: RelativeLayout
+        var menuContainer: ConstraintLayout
         var menuItem: TextView
 
         init {
